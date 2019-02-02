60c4ad50f4963eb307c4ed8ba2d89ff9a20feaa3
Enrico Giurin <enricogiurin@gmail.com>
2019-02-02 19:58:22 +0100

    Using assertJ for testing

--- lambico-datatest-jpa/src/test/java/org/lambico/datatest/example1/LoadTest.java
+++ lambico-datatest-jpa/src/test/java/org/lambico/datatest/example1/LoadTest.java
@@ -1,21 +1,19 @@
 package org.lambico.datatest.example1;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.lambico.datatest.example1.model.Entity1;
+import org.lambico.datatest.example1.model.Entity2;
+import org.lambico.datatest.json.DataAggregator;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.junit.Test;
-import org.lambico.datatest.example1.model.Entity1;
-import org.lambico.datatest.example1.model.Entity2;
-import org.lambico.datatest.json.DataAggregator;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class LoadTest {
 
@@ -33,10 +31,11 @@ public class LoadTest {
         Entity1 entity1 = ((Entity1)entities1.iterator().next());
         Collection<?> entities2 = dataAggregator.getObjects().get("org.lambico.datatest.example1.model.Entity2");
         Entity2 entity2 = ((Entity2)entities2.iterator().next());
-        assertEquals("test1", entity1.getStringField());
-        assertEquals("test2", entity2.getStringField());
-        assertSame(entity2, entity1.getEntity2());
-        assertSame(entity1, entity2.getEntity1());
+        assertThat(entity1.getStringField()).isEqualTo("test1");
+        assertThat(entity2.getStringField()).isEqualTo("test2");
+        assertThat(entity2).isEqualTo(entity1.getEntity2());
+        assertThat(entity1).isEqualTo(entity2.getEntity1());
+
     }
     
 }
